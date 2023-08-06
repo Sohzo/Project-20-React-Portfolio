17a923c87108634df5e8e64072cb12c9f56328f6
Jonathan Winter <jonpw2004@outlook.com>
2023-08-06 17:25:38 -0600

    Created folders to organize css

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,21 +1,16 @@
 import React from 'react';
+import "./NavTabs.css";
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </a>
-      </li>
+    <ul className="nav nav-tabs bg-dark"> 
+      <p
+        className="navbar-name"
+      >
+        Jonathan Winter
+      </p>
       <li className="nav-item">
         <a
           href="#about"
@@ -23,17 +18,17 @@ function NavTabs({ currentPage, handlePageChange }) {
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
         >
-          About
+          About Me
         </a>
       </li>
       <li className="nav-item">
         <a
-          href="#blog"
-          onClick={() => handlePageChange('Blog')}
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
         >
-          Blog
+          Portfolio
         </a>
       </li>
       <li className="nav-item">
@@ -44,6 +39,17 @@ function NavTabs({ currentPage, handlePageChange }) {
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
         >
           Contact
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          // This is a conditional (ternary) operator that checks to see if the current page is "Resume"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
         </a>
       </li>
     </ul>
---
+++ src/components/pages/About/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./About.css";
+
+export default function About() {
+  return (
+    <div className="About-page">
+      <h1 className="About-title">About Me</h1>
+      <p className="About-text">
+        Welcome to my page! My name is Jonathan. I've been studying web developmenty at the University of Utah for 6 months now. 
+        I began coding after hearing that some of my friends had decided to give it a try, after some time I grew to enjoy it.
+        After a couple weeks into the bootcamp, I decided to stick with it due to the satisfaction of building successful code.
+        I believe that my strength in logical thinking will allow me to continue learning and honing my skill at the craft.
+
+        Outside of web development, I enjoy physical activities such as competitive power-lifting and MMA fighting, though I can find fun in most sports.
+        If you need a new junior web dev, feel free to contact me!  
+      </p>
+    </div>
+  );
+}
