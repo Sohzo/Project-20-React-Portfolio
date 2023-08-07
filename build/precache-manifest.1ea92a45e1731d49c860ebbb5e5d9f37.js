ec8929e57b2d59b9eaca338d26c64c612446f7fc
Jonathan Winter <jonpw2004@outlook.com>
2023-08-06 21:48:10 -0600

    Bug fixes

---
+++ build/precache-manifest.1ea92a45e1731d49c860ebbb5e5d9f37.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a8950e6ec0e9db5140f",
+    "url": "/Project-20-React-Portfolio/static/css/main.038fc7c0.chunk.css"
+  },
+  {
+    "revision": "6a8950e6ec0e9db5140f",
+    "url": "/Project-20-React-Portfolio/static/js/main.6a8950e6.chunk.js"
+  },
+  {
+    "revision": "07b6e01bb2d0a2d2f8e0",
+    "url": "/Project-20-React-Portfolio/static/css/1.d6d4d5f0.chunk.css"
+  },
+  {
+    "revision": "07b6e01bb2d0a2d2f8e0",
+    "url": "/Project-20-React-Portfolio/static/js/1.07b6e01b.chunk.js"
+  },
+  {
+    "revision": "76b67210bfe2ec6222c1",
+    "url": "/Project-20-React-Portfolio/static/js/runtime~main.76b67210.js"
+  },
+  {
+    "revision": "852f1a832abfec8624614d3848d2fdb6",
+    "url": "/Project-20-React-Portfolio/static/media/Profilepic.852f1a83.jpg"
+  },
+  {
+    "revision": "698176b90b3e61786d766d75b06a8242",
+    "url": "/Project-20-React-Portfolio/static/media/api_318-699563.698176b9.webp"
+  },
+  {
+    "revision": "298821c92d43211ed1eb8a07f42a0160",
+    "url": "/Project-20-React-Portfolio/static/media/1_FVtCyRdJ6KOr4YswTtwMeA.298821c9.webp"
+  },
+  {
+    "revision": "093ae1262ddaba006fb2810585e7b277",
+    "url": "/Project-20-React-Portfolio/static/media/depositphotos_30436145-stock-photo-e-commerce-shopping-online.093ae126.webp"
+  },
+  {
+    "revision": "080f8b6866bfaedbac3fb9ae4aa5f18a",
+    "url": "/Project-20-React-Portfolio/static/media/sociall-networking.080f8b68.webp"
+  },
+  {
+    "revision": "b64cd618488488f0e67445a626a584ca",
+    "url": "/Project-20-React-Portfolio/static/media/10-Best-Text-Editors-For-Mac-You-Can-Use.b64cd618.webp"
+  },
+  {
+    "revision": "989641ba612561f3228ed464920ce4b9",
+    "url": "/Project-20-React-Portfolio/static/media/employees-community-image_csp22496979.989641ba.jpg"
+  },
+  {
+    "revision": "5ee26ac427b68164b4a283f35ae1367a",
+    "url": "/Project-20-React-Portfolio/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+var ghpages = require('gh-pages');
+
+ghpages.publish('dist', function(err) {});
