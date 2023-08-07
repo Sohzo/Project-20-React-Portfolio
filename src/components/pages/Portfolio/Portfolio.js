05c546f91f9ad2907265d6279d6b82026ebc70a1
Jonathan Winter <jonpw2004@outlook.com>
2023-08-06 19:19:00 -0600

    Finished portfolio page

--- src/components/pages/Portfolio/Portfolio.js
+++ src/components/pages/Portfolio/Portfolio.js
@@ -1,24 +1,51 @@
 import React from 'react';
+import "./Portfolio.css";
+import apiimg from "./images/api_318-699563.webp"
+import mernimg from "./images/1_FVtCyRdJ6KOr4YswTtwMeA.webp"
+import ecomimg from "./images/depositphotos_30436145-stock-photo-e-commerce-shopping-online.webp"
+import socialimg from "./images/sociall-networking.webp";
+import jateimg from "./images/10-Best-Text-Editors-For-Mac-You-Can-Use.webp";
+import emptrack from "./images/employees-community-image_csp22496979.jpg";
 
 export default function Portfolio() {
   return (
-    <div>
-      <h1>Portfolio</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+    <div className='page'>
+      <h1 className='title'>Portfolio</h1>
+      <div className='workcolumns'>
+        <ul className='columns'>
+          <li className='coltitles'>
+            Group work
+          </li>
+          <a href="https://github.com/13Blackmagic/Mixpedia">
+            <img src={mernimg} className='imgs'></img>
+          </a>
+          <a href="https://github.com/ericklea/main-course-home-page-project">
+            <img src={apiimg} className='imgs'></img>
+          </a>
+          <a href="https://github.com/Sohzo/Ecommerce-Website-CodeCrusaders">
+            <img src={ecomimg} className='imgs'></img>
+          </a>
+        </ul>
+        <ul className='columns'>
+          <li className='coltitles'>
+            Solo Work
+          </li>
+          <a href="https://github.com/Sohzo/Social-Network-API">
+            <img src={socialimg} className='imgs'></img>
+          </a>
+          <a href="https://github.com/Sohzo/Project-19-Text-Editor">
+            <img src={jateimg} className='imgs'></img>
+          </a>
+          <a href="https://github.com/Sohzo/Employee-Tracker">
+            <img src={emptrack} className='imgs'></img>
+          </a>
+        </ul>
+      </div>
+
+
+
+
+
     </div>
   );
 }
