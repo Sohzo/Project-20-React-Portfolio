3ac79422800a6f324c78f91ed5091335e0b188bf
Jonathan Winter <jonpw2004@outlook.com>
2023-08-06 20:26:48 -0600

    Added contact me form

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -5,6 +5,16 @@ export default function Contact() {
   return (
     <div className='page'>
       <h1 className='title'>Contact Me</h1>
+
+      <form className='container'>
+        <h3>Get in touch</h3>
+        <input type="text" id="name" placeholder='Your Name' required></input>
+        <input type="email" id="email" placeholder='Email' required></input>
+        <textarea id="message" rows="5" placeholder="Message"></textarea>
+        <button type="submit">Submit</button>
+
+      </form>
+      
       <ul>
         <li className="text">Email: jonpw2004@outlook.com</li>
         <li className="text">Phone#: 801-935-0042</li>
